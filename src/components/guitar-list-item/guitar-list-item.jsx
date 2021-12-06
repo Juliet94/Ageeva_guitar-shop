ba8a017accb312a149dd9edd50f569be15fb05dc
Anastasia <70826807+Juliet94@users.noreply.github.com>
2021-12-06 10:28:09 +0300

    Уберет консоль

--- src/components/guitar-list-item/guitar-list-item.jsx
+++ src/components/guitar-list-item/guitar-list-item.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import React, {useState, useEffect} from 'react';
 import {useDispatch} from 'react-redux';
 import {Link, generatePath} from 'react-router-dom';
@@ -23,12 +22,10 @@ export default function GuitarListItem({guitarItem}) {
   useEffect(() => {
     if (isModalOpen || isModalSuccessOpen) {
       document.body.style = 'overflow: hidden;';
-      console.log('Хоба!')
     }
 
     if (!isModalOpen && !isModalSuccessOpen) {
       document.body.style = 'overflow: visible;';
-      console.log('Еще Хоба!')
     }
   }, [isModalOpen, isModalSuccessOpen]);
 
